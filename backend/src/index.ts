2c94dd6797f32c24a0bf335c8e5fa63ad5ffb1b6
yanghd <yanghd@vt.edu>
2023-12-30 23:59:38 -0500

    ops: set up connection with mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,24 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
+
+// Set up the connection with mongo db
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => console.log("Connect to MongoDB database successfully"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
 // Parse incoming json request and url-encoded data
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); // 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //
 
 // Allow cross origin resources sharing (cors)
 app.use(cors());
 
 // Server run on port 3000
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
-
+  console.log("Server is running on port 3000");
+});
