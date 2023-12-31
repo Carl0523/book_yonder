e0fb9ad3fe7b553b2a9385ecdf35df129bd8ea09
yanghd <yanghd@vt.edu>
2023-12-31 15:46:04 -0500

    feat: create a reusable button component

---
+++ frontend/src/components/utils/CustomButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+interface CustomButtonProps {
+  text: string;
+  textColor?: string;
+  bgColor?: string;
+  responsiveWidth?: string;
+  padding?: string;
+  type?: "button" | "submit" | "reset";
+  buttonHandler?: () => void;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  text,
+  textColor = "text-blue-100",
+  bgColor = "bg-primary",
+  responsiveWidth = "w-24",
+  padding = "py-2 px-4",
+  type = "button",
+  buttonHandler = undefined
+}) => {
+  return (
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      type={type}
+      onClick={buttonHandler}
+      className={`${responsiveWidth} ${padding} ${textColor} ${bgColor} rounded-buttonRadius font-semibold`}
+    >
+      {text}
+    </motion.button>
+  );
+};
+
+export default CustomButton;
