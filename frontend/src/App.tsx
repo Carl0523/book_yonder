b8e9a3c70e8c9f7fa7fdba952e72e7d8b289d8c9
yanghd <yanghd@vt.edu>
2023-12-31 15:45:02 -0500

    feat: Set up the routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <h1>hello</h1>
-  )
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
-export default App
+export default App;
