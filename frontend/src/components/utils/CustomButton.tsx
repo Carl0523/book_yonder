a6dd91054213f5dd8d4de5ed1e5efb6b3dd1667c
yanghd <yanghd@vt.edu>
2023-12-31 23:55:19 -0500

    style: Add some extra hover effect

--- frontend/src/components/utils/CustomButton.tsx
+++ frontend/src/components/utils/CustomButton.tsx
@@ -18,15 +18,15 @@ const CustomButton: React.FC<CustomButtonProps> = ({
   responsiveWidth = "w-24",
   padding = "py-2 px-4",
   type = "button",
-  buttonHandler = undefined
+  buttonHandler = undefined,
 }) => {
   return (
     <motion.button
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: 1.05, opacity: 0.9 }}
       whileTap={{ scale: 0.95 }}
       type={type}
       onClick={buttonHandler}
-      className={`${responsiveWidth} ${padding} ${textColor} ${bgColor} rounded-buttonRadius font-semibold`}
+      className={`${responsiveWidth} ${padding} ${textColor} ${bgColor} rounded-buttonRadius font-semibold active:`}
     >
       {text}
     </motion.button>
