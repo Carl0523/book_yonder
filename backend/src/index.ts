0fa4f59180b4b3077b225985f50503d99b990804
yanghd <yanghd@vt.edu>
2024-01-01 22:54:27 -0500

    feat: Add router for auth and a error middleware handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
+
+import authRouter from "./routes/auth.route";
 
 // Set up the connection with mongo db
 mongoose
@@ -17,6 +20,21 @@ app.use(express.urlencoded({ extended: true })); //
 
 // Allow cross origin resources sharing (cors)
 app.use(cors());
+app.use(cookieParser());
+
+// Set up routers
+app.use("/api/auth", authRouter);
+
+// Middleware function to handle the error
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  const errorMessage = err.errorMessage || "Internal Server Error";
+  res.status(statusCode).json({
+    message: errorMessage,
+    statusCode,
+  });
+  next();
+});
 
 // Server run on port 3000
 app.listen(3000, () => {
