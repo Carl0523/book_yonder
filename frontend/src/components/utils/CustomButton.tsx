00e3b746ec41edc4d6f68742afab2ed3085b9871
yanghd <yanghd@vt.edu>
2024-01-03 23:21:28 -0500

    style: Add icon and customcss props

--- frontend/src/components/utils/CustomButton.tsx
+++ frontend/src/components/utils/CustomButton.tsx
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
+
 
 interface CustomButtonProps {
   text: string;
+  icon?: ReactNode;
   textColor?: string;
   bgColor?: string;
   responsiveWidth?: string;
   padding?: string;
   type?: "button" | "submit" | "reset";
+  customCSS?: string,
   buttonHandler?: () => void;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   text,
+  icon=undefined,
   textColor = "text-blue-100",
   bgColor = "bg-primary",
   responsiveWidth = "w-24",
   padding = "py-2 px-4",
   type = "button",
+  customCSS= "",
   buttonHandler = undefined,
 }) => {
   return (
     <motion.button
-      whileHover={{ scale: 1.05, opacity: 0.9 }}
+      whileHover={{ scale: 1.02, opacity: 0.9 }}
       whileTap={{ scale: 0.95 }}
       type={type}
       onClick={buttonHandler}
-      className={`${responsiveWidth} ${padding} ${textColor} ${bgColor} rounded-buttonRadius font-semibold active:`}
+      className={`flex justify-center items-center gap-4 ${responsiveWidth} ${padding} ${textColor} ${bgColor} rounded-buttonRadius font-semibold active: ${customCSS}`}
     >
+      {icon}
       {text}
     </motion.button>
   );
