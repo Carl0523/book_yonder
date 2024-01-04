17cd9ebb76d3610dd2a19de44daed111ed5f0a78
yanghd <yanghd@vt.edu>
2024-01-03 23:57:38 -0500

    feat: Add and implemented user logout api

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,6 +3,13 @@ import User from "../models/user.model";
 import bcryptjs from "bcryptjs";
 import generateToken from "../utils/generateToken";
 
+
+/**
+ * Handle user registration requests by processing them, storing a cookie, and saving the user information in the database
+ * @param req The POST request from client: contains email, password, firstName, lastName 
+ * @param res response client with userInfo exclude the password
+ * @param next next function to handle the error
+ */
 const userRegister = async (
   req: Request,
   res: Response,
@@ -40,6 +47,12 @@ const userRegister = async (
   }
 };
 
+/**
+ * Handle user's login request by processing them, storing a cookie
+ * @param req The POST request from client: containing email and password
+ * @param res Response the client with userInfo excluding the password
+ * @param next next function to handle the error
+ */
 const userLogin = async (req: Request, res: Response, next: NextFunction) => {
   const {email, password} = req.body;
 
@@ -63,4 +76,21 @@ const userLogin = async (req: Request, res: Response, next: NextFunction) => {
   }
 }
 
-export { userRegister, userLogin };
+/**
+ * Log out the user by remove the access_token cookie
+ * @param req the POST request from client side
+ * @param res response a simple message of logging out
+ * @param next next function to handle the error
+ */
+const userLogout = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.cookie("access_token", "", {
+      expires: new Date(0)
+    });
+    res.status(200).json({message : "User logout successfully"})
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { userRegister, userLogin, userLogout };
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import {userRegister, userLogin} from "../controllers/auth.controller";
+import {userRegister, userLogin, userLogout} from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/register", userRegister);
 router.post("/login", userLogin);
+router.post("/logout", userLogout);
 
 
 
