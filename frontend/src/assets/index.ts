fa19ff056ef64ebec3f2de5b43b9ca20b4879b18
yanghd <yanghd@vt.edu>
2024-01-04 22:50:18 -0500

    style: Upload login image

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,8 +1,9 @@
 import logo from "./logo.svg"
 import hero from "./hero.jpg";
 import registerImage from "./register2.jpg";
+import loginImage from "./login.jpg";
 
 
 
 
-export {logo, hero, registerImage};
+export {logo, hero, registerImage, loginImage};
