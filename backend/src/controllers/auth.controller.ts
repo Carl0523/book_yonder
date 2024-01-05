1cc8c5988a74838eca81e461d5e400491895f8cd
yanghd <yanghd@vt.edu>
2024-01-04 22:30:35 -0500

    docs: Add comments to the api functions

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,10 +3,9 @@ import User from "../models/user.model";
 import bcryptjs from "bcryptjs";
 import generateToken from "../utils/generateToken";
 
-
 /**
  * Handle user registration requests by processing them, storing a cookie, and saving the user information in the database
- * @param req The POST request from client: contains email, password, firstName, lastName 
+ * @param req The POST request from client: contains email, password, firstName, lastName
  * @param res response client with userInfo exclude the password
  * @param next next function to handle the error
  */
@@ -22,26 +21,29 @@ const userRegister = async (
 
     // Return error if user existed
     if (existedUser) {
-      return res.status(500).json({ message: "User is already existed" });
+      return res
+        .status(500)
+        .json({ message: "Email already exist, try login with this email." });
     }
 
     // Hashed the user entered password
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
     // Create and saved new User to MongoDB database
-    const newUser = new User({firstName, lastName, email, password: hashedPassword});
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
 
     // Generate token and store in cookie
     generateToken(res, newUser._id);
 
-    const {password: userPassword, ...rest} = newUser._doc;
+    const { password: userPassword, ...rest } = newUser._doc;
 
-
-    
     res.status(201).json(rest);
-
-
   } catch (error) {
     next(error);
   }
@@ -54,27 +56,28 @@ const userRegister = async (
  * @param next next function to handle the error
  */
 const userLogin = async (req: Request, res: Response, next: NextFunction) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-    if (!user) return res.status(404).json({message: "Invalid Email or Password"});
+    if (!user)
+      return res.status(404).json({ message: "Invalid Email or Password" });
 
     const isPasswordMatched = await bcryptjs.compare(password, user.password);
 
-    if (!isPasswordMatched) return res.status(401).json({message: "Invalid Email or Password"});
+    if (!isPasswordMatched)
+      return res.status(401).json({ message: "Invalid Email or Password" });
 
     generateToken(res, user._id);
 
-    const {password: userPassword, ...rest} = user._doc;
+    const { password: userPassword, ...rest } = user._doc;
 
     res.status(201).json(rest);
-
   } catch (error) {
     next(error);
   }
-}
+};
 
 /**
  * Log out the user by remove the access_token cookie
@@ -85,12 +88,12 @@ const userLogin = async (req: Request, res: Response, next: NextFunction) => {
 const userLogout = async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.cookie("access_token", "", {
-      expires: new Date(0)
+      expires: new Date(0),
     });
-    res.status(200).json({message : "User logout successfully"})
+    res.status(200).json({ message: "User logout successfully" });
   } catch (error) {
     next(error);
   }
-}
+};
 
 export { userRegister, userLogin, userLogout };
