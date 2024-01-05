e8b17835d0bddacbe68f5984fd172cc0d2681794
yanghd <yanghd@vt.edu>
2024-01-04 22:43:32 -0500

    style: wrap screens with AnimatePresence

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <Outlet />
-      <Footer/>
+      <AnimatePresence mode="wait">
+        <Outlet />
+      </AnimatePresence>
+      <Footer />
     </div>
   );
 }
