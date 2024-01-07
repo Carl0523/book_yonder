e238ccfaec81b9f3ebd528349ad778dfebd05c29
yanghd <yanghd@vt.edu>
2024-01-07 00:38:06 -0500

    style: Make it responsive

--- frontend/src/components/homepage/Hero.tsx
+++ frontend/src/components/homepage/Hero.tsx
@@ -12,10 +12,10 @@ const Hero = () => {
       />
       {/* 2. The Slogan and start explore button */}
       <div className="absolute xs:top-1/2 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-5 text-center text-white font-bold">
-        <h1 className="xs:text-5xl text-3xl">
+        <h1 className="md:text-5xl sm:text-4xl text-3xl">
           Book Your Stay & Make You the Most Comfort Travel
         </h1>
-        <h2 className="xs:text-2xl text-lg"> The best hotels for you</h2>
+        <h2 className="md:text-2xl sm:text-xl text-lg"> The best hotels for you</h2>
         <CustomButton text="Explore more" responsiveWidth="w-30" />
       </div>
     </div>
