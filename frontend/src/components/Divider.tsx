7423dbdd5436b52bfd05345526835f958d9816fd
yanghd <yanghd@vt.edu>
2024-01-07 00:36:14 -0500

    feat: Add a divider component

---
+++ frontend/src/components/Divider.tsx
@@ -0,0 +1,12 @@
+
+const Divider = () => {
+  return (
+    <div className="w-full my-2 flex gap-2 items-center ">
+      <hr className="flex-1 border-t-2" />
+      <span className="text-gray-400">OR</span>
+      <hr className="flex-1 border-t-2" />
+    </div>
+  );
+};
+
+export default Divider;
