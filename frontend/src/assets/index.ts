f5f6b904a981732c76a876b13a4d1c1ecedd41a6
yanghd <yanghd@vt.edu>
2024-01-11 01:42:53 -0500

    style: add delete account svg

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,8 +2,9 @@ import logo from "./logo.svg"
 import hero from "./hero.jpg";
 import registerImage from "./register2.jpg";
 import loginImage from "./login.jpg";
+import deleteAccount from "./delete_account.svg";
 
 
 
 
-export {logo, hero, registerImage, loginImage};
+export {logo, hero, registerImage, loginImage, deleteAccount};
