5fe83a7f98d733ed9a46e7ba93dceee3c876a538
yanghd <yanghd@vt.edu>
2024-01-22 13:16:59 -0500

    chore: Import and export the svg for empty property

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -3,8 +3,9 @@ import hero from "./hero.jpg";
 import registerImage from "./register2.jpg";
 import loginImage from "./login.jpg";
 import deleteAccount from "./delete_account.svg";
+import emptyProperty from "./empty_property.svg";
 
 
 
 
-export {logo, hero, registerImage, loginImage, deleteAccount};
+export {logo, hero, registerImage, loginImage, deleteAccount, emptyProperty};
