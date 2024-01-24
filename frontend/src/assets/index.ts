c7e07f90194c2c2ab10814c7615365f5fb75452e
yanghd <yanghd@vt.edu>
2024-01-24 17:51:18 -0500

    chore: Import and export image icons for property type

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,11 +1,23 @@
-import logo from "./logo.svg"
+import logo from "./logo.svg";
 import hero from "./hero.jpg";
 import registerImage from "./register2.jpg";
 import loginImage from "./login.jpg";
 import deleteAccount from "./delete_account.svg";
 import emptyProperty from "./empty_property.svg";
+import apartment from "./apartment.png";
+import home from "./home.png";
+import hotel from "./hotel.png";
+import alternative from "./camping.png";
 
-
-
-
-export {logo, hero, registerImage, loginImage, deleteAccount, emptyProperty};
+export {
+  logo,
+  hero,
+  registerImage,
+  loginImage,
+  deleteAccount,
+  emptyProperty,
+  apartment,
+  home,
+  hotel,
+  alternative,
+};
